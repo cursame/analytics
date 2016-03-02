898c5cda7cc7d94552462d28e4e2414e32a2f03e
Carlos Cessa <carlos@pixative.com>
2016-03-02 13:56:13 -0600

    Database Initialization
    - Added support to initialize the connection with the MongoDB database
    upon starting of the server

---
+++ config/db.example.js
@@ -0,0 +1,7 @@
+module.exports  = {
+    'database'  : 'MONGODB_DATABASE',
+    'host'      : 'MONGODB_HOST',
+    'pass'      : 'MONGODB_USER_PASSWORD',
+    'port'      : 'MONGODB_PORT',
+    'user'      : 'MONGODB_USER'
+};
