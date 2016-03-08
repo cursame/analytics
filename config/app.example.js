766df65cfef7ed4e1647d8a02d03f483e391b7c3
Carlos Cessa <carlos@pixative.com>
2016-03-08 12:48:45 -0600

    Application Configuration
    - Modified application configuration file to include parameters for
    the unit testing application object and request validation

--- config/app.example.js
+++ config/app.example.js
@@ -1,3 +1,6 @@
 module.exports  = {
-    encryption_key  : 'APPLICATION_32_CHARACTERS_ENCRYPTION_KEY'
+    encryption_key      : 'APPLICATION_32_CHARACTERS_ENCRYPTION_KEY',
+    request_lifespan    : LIFESPAN_IN_MILLISECONDS_FOR_REQUESTS,
+    test_app_id         : 'UNITTEST_APPLICATION_ID',
+    test_app_secret     : 'UNITTEST_APPLICATION_SECRET'
 };
