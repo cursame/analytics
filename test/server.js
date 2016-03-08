9871222bd0494de759072f7832c7adbb3ed27cf2
Carlos Cessa <carlos@pixative.com>
2016-03-08 14:11:00 -0600

    Applications Testing
    - Added unit test describers to evaluate the added functionality

--- test/server.js
+++ test/server.js
@@ -1,4 +1,5 @@
 var assert      = require( 'assert' ),
+    crypto      = require( 'crypto' ),
     should      = require( 'should' ),
     request     = require( 'supertest' ),
     server      = require( '../server' ),
@@ -71,5 +72,139 @@ describe( 'Sessions', function () {
             .delete( '/sessions/' + session )
             .send( Auth.sign() )
             .expect( 200, done );
+    });
+});
+
+describe( 'Applications', function () {
+    var application     = {
+            name        : 'UnitTestApplication',
+            description : 'Application created from the unit test environment to verify the applications resource.',
+            permissions : [ 'sessions_write', 'users_write', 'users_read', 'rooms_write', 'rooms_read', 'hotels_write', 'hotels_read', 'reports_write', 'reports_read', 'complains_write', 'complains_read', 'clients_write', 'clients_read', 'products_write', 'products_read', 'sells_write', 'sells_read', 'purchases_write', 'purchases_read', 'payments_write', 'payments_read', 'shifts_write', 'shifts_read', 'applications_write', 'applications_read' ]
+        },
+        incomplete      = {
+            name        : 'IncompleteUnitTestApplication',
+            description : 'Application created from the unit test environment without full permissions to check the client authentication.',
+            permissions : [ 'sessions_write', 'users_write', 'users_read', 'rooms_write', 'rooms_read', 'hotels_write', 'hotels_read', 'reports_write', 'reports_read', 'complains_write', 'complains_read', 'clients_write', 'clients_read', 'products_write', 'products_read', 'sells_write', 'sells_read', 'purchases_write', 'purchases_read', 'payments_write', 'payments_read' ]
+        },
+        creation        = "",
+        secret          = "",
+        incomplete_id   = "",
+        inc_secret      = "";
+
+    it ( 'create a new application in the system', function ( done ) {
+        request( server )
+            .post( '/applications' )
+            .send( Auth.sign( application ) )
+            .end( function ( err, res ) {
+                if ( err ) {
+                    throw err;
+                }
+
+                res.body.should.have.property( '_id' );
+                res.body.should.have.property( 'name' );
+                res.body.should.have.property( 'creation_date' );
+                res.body.should.have.property( 'description' );
+                res.body.should.have.property( 'permissions' );
+                res.body.should.have.property( 'secret' );
+
+                app         = res.body._id;
+                secret      = res.body.secret;
+                creation    = res.body.creation_date;
+
+                done();
+            });
+    });
+
+    it ( 'get a 403 error when attempting to create an invalid application', function ( done ) {
+        request( server )
+            .post( '/applications' )
+            .send( Auth.sign() )
+            .expect( 403, done );
+    });
+
+    it ( 'update the previously created application without modifying the creation date', function ( done ) {
+        request( server )
+            .put( '/applications/' + app )
+            .send( Auth.sign( { name : 'UnitTestApp', creation_date : Date.now() }) )
+            .end( function ( err, res ) {
+                if ( err ) {
+                    throw err;
+                }
+
+                res.body.should.have.property( '_id' );
+                res.body.should.have.property( 'name' );
+                res.body.should.have.property( 'creation_date' );
+                res.body.should.have.property( 'description' );
+                res.body.should.have.property( 'permissions' );
+                res.body.should.have.property( 'secret' );
+
+                assert.equal( 'UnitTestApp', res.body.name );
+                assert.equal( secret, res.body.secret );
+                assert.equal( creation, res.body.creation_date );
+
+                done();
+            });
+    });
+
+    it ( 'get a 404 error when attempting to update an invalid application', function ( done ) {
+        request( server )
+            .put( '/applications/not_there' )
+            .send( Auth.sign() )
+            .expect( 404, done );
+    });
+
+    it ( 'create a new application in the system', function ( done ) {
+        request( server )
+            .post( '/applications' )
+            .send( Auth.sign( incomplete ) )
+            .end( function ( err, res ) {
+                if ( err ) {
+                    throw err;
+                }
+
+                incomplete_id   = res.body._id;
+                inc_secret      = res.body.secret;
+                done();
+            });
+    });
+
+    it ( 'get a 401 error when attempting to access an unauthorized resource', function ( done ) {
+        var time        = new Date().getTime(),
+            hash        = crypto.createHash( 'sha1' );
+        hash.update( time + inc_secret );
+
+        var signature   = {
+                consumer    : incomplete_id,
+                timestamp   : time,
+                signature   : hash.digest( 'hex' )
+            };
+
+        request( server )
+            .get( '/applications' )
+            .send( signature )
+            .expect( 401, done );
+    });
+
+    it ( 'remove the previously created application', function ( done ) {
+        request( server )
+            .delete( '/applications/' + incomplete_id )
+            .send( Auth.sign() )
+            .expect( 200, done );
+    });
+});
+
+describe( 'Applications Removal', function () {
+    it ( 'remove the previously created application', function ( done ) {
+        request( server )
+            .delete( '/applications/' + app )
+            .send( Auth.sign() )
+            .expect( 200, done );
+    });
+
+    it ( 'get a 404 error when attempting to remove an invalid application', function ( done ) {
+        request( server )
+            .delete( '/applications/not_there' )
+            .send( Auth.sign() )
+            .expect( 404, done );
     });
 });
