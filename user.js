4374f5438c50baf37e1c6ae08ae3d583d230e7b6
Carlos Cessa <carlos@pixative.com>
2016-03-08 00:15:49 -0600

    Users Helper
    - Modified user helper to be able to set an external id

--- user.js
+++ user.js
@@ -3,8 +3,9 @@ var mongoose    = require( 'mongoose' ),
     User        = require( './models/user' ),
     name        = process.argv[2],
     email       = process.argv[3],
-    pass        = process.argv[4],
-    type        = process.argv[5],
+    ext_id      = process.argv[4],
+    pass        = process.argv[5],
+    type        = process.argv[6],
     conn_str    = 'mongodb://';
 
 if ( db.user ) {
@@ -20,17 +21,20 @@ conn_str        += db.host + ':' + db.port + '/' + db.database;
 mongoose.connect( conn_str );
 
 User.create({
-    email   : email,
-    name    : name,
-    pass    : pass,
-    type    : type
+    email       : email,
+    external_id : ext_id,
+    name        : name,
+    pass        : pass,
+    type        : type
 }, function ( err, user ) {
     if ( err || !user ) {
         console.log( '============= ERROR ==============' );
+        console.log( err );
     } else {
         console.log( '============= USER ==============' );
         console.log( 'Name - ' + user.name );
         console.log( 'Email - ' + user.email );
+        console.log( 'External ID - ' + user.external_id );
         console.log( 'Pass - ' + pass );
         console.log( 'Type - ' + user.type );
     }
