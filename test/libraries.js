db8457c46a66781a206d3c8e9ea1f06cdb9381f7
Carlos Cessa <carlos@pixative.com>
2016-03-08 12:55:17 -0600

    Libraries Testing
    - Created script to perform unit tests in the application libraries

---
+++ test/libraries.js
@@ -0,0 +1,52 @@
+var assert  = require( 'assert' ),
+    should  = require( 'should' ),
+    Encrypt = require( '../lib/encrypt' ),
+    request = require( 'supertest' ),
+    server  = require( '../server' ),
+    String  = require( '../lib/string' );
+
+describe( 'AnalyticsAPI Libraries', function () {
+    var original    = "",
+        encoded     = "";
+
+    it ( 'generate a random string of 12 characters', function ( done ) {
+        original    = String.random( 12, "alnum" );
+
+        assert.equal( original.length, 12 );
+        done();
+    });
+
+    it ( 'encode the random string generated', function ( done ) {
+        encoded     = Encrypt.encode( original );
+
+        done();
+    });
+
+    it ( 'decode the previously generated string', function ( done ) {
+        var decoded = Encrypt.decode( encoded );
+
+        assert.equal( original, decoded );
+        done();
+    });
+
+    it ( 'get an empty string when attempting to decode an invalid cipher', function ( done ) {
+        assert.equal( "", Encrypt.decode( "invalid" ) );
+        done();
+    });
+
+    it ( 'generate a random numeric string of 10 characters', function ( done ) {
+        var random  = String.random( 10, 'numeric' );
+
+        assert.equal( random.length, 10 );
+        assert.equal( /^[0-9]+$/.test( random ), true );
+        done();
+    });
+
+    it ( 'generate a random alpha string of 10 characters', function ( done ) {
+        var random  = String.random( 10, 'alpha' );
+
+        assert.equal( random.length, 10 );
+        assert.equal( /^[A-z]+$/.test( random ), true );
+        done();
+    });
+});
