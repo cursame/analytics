3ee58e1a1e5ddd5cb9143cfd1ba070f852f76001
Carlos Cessa <carlos@pixative.com>
2016-03-11 14:59:24 -0600

    Results List
    - Included method in the utils library to perform paginated queries in
    the application records
    - Modified application configuration file to include a parameter for
    the default pagition results page size

--- config/app.example.js
+++ config/app.example.js
@@ -1,5 +1,6 @@
 module.exports  = {
     encryption_key      : 'APPLICATION_32_CHARACTERS_ENCRYPTION_KEY',
+    page_size           : NUMBER_OF_RECORDS_TO_RETURN_IN_GET_QUERIES,
     request_lifespan    : LIFESPAN_IN_MILLISECONDS_FOR_REQUESTS,
     test_app_id         : 'UNITTEST_APPLICATION_ID',
     test_app_secret     : 'UNITTEST_APPLICATION_SECRET'
