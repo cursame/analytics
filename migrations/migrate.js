48f8091906fd9675d3e07c5136824595528690ba
Carlos Cessa <carlos@pixative.com>
2016-03-19 20:53:42 -0600

    Database Migrations
    - Modified migrations library to use the new `Assignments` migration
    to generate random assignments records

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -1,10 +1,12 @@
-var async           = require( 'async' ),
-    Courses         = require( './courses' ),
-    Users           = require( './users' ),
-    Utils           = require( '../lib/utils' );
-    numberStudents  = process.argv[2],
-    numberTeachers  = process.argv[3],
-    numberCourses   = process.argv[4];
+var async               = require( 'async' ),
+    Assignments         = require( './assignments' ),
+    Courses             = require( './courses' ),
+    Users               = require( './users' ),
+    Utils               = require( '../lib/utils' );
+    numberStudents      = process.argv[2],
+    numberTeachers      = process.argv[3],
+    numberCourses       = process.argv[4],
+    numberAssignments   = process.argv[5];
 
 Utils.connectDB();
 
@@ -35,10 +37,20 @@ async.series([
         } else {
             callback( null, 0 );
         }
+    },
+    function ( callback ) {
+        if ( numberAssignments !== undefined && numberAssignments > 0 ) {
+            Assignments.create( numberAssignments, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
     }
 ], function ( err, results ) {
     console.log( 'Students created: ' + results[0] );
     console.log( 'Teachers created: ' + results[1] );
     console.log( 'Courses created: ' + results[2] );
+    console.log( 'Assignments created: ' + results[3] );
     process.exit();
 });
