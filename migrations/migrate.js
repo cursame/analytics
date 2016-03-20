9de70a69b46bdfa023763fcc7df8404cb7fbe7e9
Carlos Cessa <carlos@pixative.com>
2016-03-19 20:22:38 -0600

    Database Migrations
    - Modified migrations library to use the new `Courses` migration to
    generate random courses in the database

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -1,10 +1,44 @@
-var Users           = require( './users' ),
+var async           = require( 'async' ),
+    Courses         = require( './courses' ),
+    Users           = require( './users' ),
     Utils           = require( '../lib/utils' );
-    numberStudents  = process.argv[2];
+    numberStudents  = process.argv[2],
+    numberTeachers  = process.argv[3],
+    numberCourses   = process.argv[4];
 
 Utils.connectDB();
 
-Users.create( 2, numberStudents, function ( total ) {
-    console.log( 'Students created: ' + numberStudents );
+async.series([
+    function ( callback ) {
+        if ( numberStudents !== undefined && numberStudents > 0 ) {
+            Users.create( 2, numberStudents, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
+    },
+    function ( callback ) {
+        if ( numberTeachers !== undefined && numberTeachers > 0 ) {
+            Users.create( 1, numberTeachers, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
+    },
+    function ( callback ) {
+        if ( numberCourses !== undefined && numberCourses > 0 ) {
+            Courses.create( numberCourses, 15, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
+    }
+], function ( err, results ) {
+    console.log( 'Students created: ' + results[0] );
+    console.log( 'Teachers created: ' + results[1] );
+    console.log( 'Courses created: ' + results[2] );
     process.exit();
 });
