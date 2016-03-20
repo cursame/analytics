ed3d9d0aa972acebcfe0352f855094f313ddbab8
Carlos Cessa <carlos@pixative.com>
2016-03-20 17:25:19 -0600

    Application Migrations
    - Added support to add comments to the application database

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -1,12 +1,14 @@
 var async               = require( 'async' ),
     Assignments         = require( './assignments' ),
+    Comments            = require( './comments' ),
     Courses             = require( './courses' ),
     Users               = require( './users' ),
     Utils               = require( '../lib/utils' );
     numberStudents      = process.argv[2],
     numberTeachers      = process.argv[3],
     numberCourses       = process.argv[4],
-    numberAssignments   = process.argv[5];
+    numberAssignments   = process.argv[5],
+    numberComments      = process.argv[6];
 
 Utils.connectDB();
 
@@ -46,11 +48,21 @@ async.series([
         } else {
             callback( null, 0 );
         }
+    },
+    function ( callback ) {
+        if ( numberComments !== undefined && numberComments > 0 ) {
+            Comments.create( numberComments, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
     }
 ], function ( err, results ) {
     console.log( 'Students created: ' + results[0] );
     console.log( 'Teachers created: ' + results[1] );
     console.log( 'Courses created: ' + results[2] );
     console.log( 'Assignments created: ' + results[3] );
+    console.log( 'Comments created: ' + results[4] );
     process.exit();
 });
