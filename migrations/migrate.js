12a83e5e833880911e1326dac1c119ff85f2dbf0
Carlos Cessa <carlos@pixative.com>
2016-03-20 20:13:04 -0600

    Application Migrations
    - Included libraries to generate discussions, files and grades in the
    application migration

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -2,13 +2,19 @@ var async               = require( 'async' ),
     Assignments         = require( './assignments' ),
     Comments            = require( './comments' ),
     Courses             = require( './courses' ),
+    Discussions         = require( './discussions' ),
+    Files               = require( './files' ),
+    Grades              = require( './grades' ),
     Users               = require( './users' ),
     Utils               = require( '../lib/utils' );
     numberStudents      = process.argv[2],
     numberTeachers      = process.argv[3],
     numberCourses       = process.argv[4],
     numberAssignments   = process.argv[5],
-    numberComments      = process.argv[6];
+    numberComments      = process.argv[6],
+    numberDiscussions   = process.argv[7],
+    numberFiles         = process.argv[8],
+    numberGrades        = process.argv[9];
 
 Utils.connectDB();
 
@@ -57,6 +63,33 @@ async.series([
         } else {
             callback( null, 0 );
         }
+    },
+    function ( callback ) {
+        if ( numberDiscussions !== undefined && numberDiscussions > 0 ) {
+            Discussions.create( numberDiscussions, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
+    },
+    function ( callback ) {
+        if ( numberFiles !== undefined && numberFiles > 0 ) {
+            Files.create( numberFiles, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
+    },
+    function ( callback ) {
+        if ( numberGrades !== undefined && numberGrades > 0 ) {
+            Grades.create( numberGrades, function ( total ) {
+                callback( null, total );
+            });
+        } else {
+            callback( null, 0 );
+        }
     }
 ], function ( err, results ) {
     console.log( 'Students created: ' + results[0] );
@@ -64,5 +97,8 @@ async.series([
     console.log( 'Courses created: ' + results[2] );
     console.log( 'Assignments created: ' + results[3] );
     console.log( 'Comments created: ' + results[4] );
+    console.log( 'Discussions created: ' + results[5] );
+    console.log( 'Files created: ' + results[6] );
+    console.log( 'Grades created: ' + results[7] );
     process.exit();
 });
