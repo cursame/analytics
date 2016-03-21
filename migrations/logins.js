8889bbb4ce116856997941eb90b48aff14c0ba4a
Carlos Cessa <carlos@pixative.com>
2016-03-20 20:30:04 -0600

    Logins Migration
    - Created library to generate random valid logins entries in the
    database

---
+++ migrations/logins.js
@@ -0,0 +1,81 @@
+var async           = require( 'async' ),
+    Chance          = require( 'chance' ),
+    Login           = require( '../models/login' ),
+    User            = require( '../models/user' ),
+    chance          = new Chance(),
+    totalUsers      = null,
+    _getUsers       = function ( cb ) {
+        if ( totalUsers === null ) {
+            User.count({
+                $or     : [
+                    {
+                        type    : 1
+                    },
+                    {
+                        type    : 2
+                    }
+                ]
+            }, function ( err, users ) {
+                totalUsers  = users;
+                cb();
+            });
+        } else {
+            cb();
+        }
+    };
+
+exports.create      = function ( total, cb ) {
+    var logins      = [],
+        iterated    = 0;
+
+    _getUsers( function () {
+        for ( var i = 0; i < total; i++ ) {
+            async.waterfall([
+                function ( callback ) {
+                    var skip    = chance.integer({
+                            min : 0,
+                            max : totalUsers - 1
+                        });
+
+                    User.findOne({
+                        $or     : [
+                            {
+                                type    : 1
+                            },
+                            {
+                                type    : 2
+                            }
+                        ]
+                    }).select( '_id type' ).skip( skip ).exec( function ( err, user ) {
+                        callback( null, user );
+                    });
+                },
+                function ( user, callback ) {
+                    var year    = new Date().getFullYear(),
+                        date    = new Date( chance.hammertime() );
+
+                    date.setYear( year );
+
+                    callback( null, user, date );
+                },
+                function ( user, date, callback ) {
+                    Login.create({
+                        date    : date.toISOString(),
+                        type    : user.type,
+                        user    : user._id
+                    }, function ( err, login ) {
+                        callback( null, login );
+                    });
+                }
+            ], function ( err, result ) {
+                if ( result ) {
+                    logins.push( result );
+                }
+
+                if ( ++iterated == total ) {
+                    cb( logins.length );
+                }
+            });
+        }
+    });
+};
