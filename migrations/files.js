129bffd4ef71e9a1fe8275e6b623e244ebab50eb
Carlos Cessa <carlos@pixative.com>
2016-03-20 19:31:03 -0600

    Files Migration
    - Created library to generate random files valid entries in the
    database

---
+++ migrations/files.js
@@ -0,0 +1,63 @@
+var async           = require( 'async' ),
+    Chance          = require( 'Chance' ),
+    Course          = require( '../models/course' ),
+    File            = require( '../models/file' ),
+    chance          = new Chance(),
+    totalCourses    = null,
+    _getCourses     = function ( cb ) {
+        if ( totalCourses === null ) {
+            Course.count( function ( err, count ) {
+                totalCourses    = count;
+                cb();
+            });
+        } else {
+            cb();
+        }
+    };
+
+exports.create      = function ( total, cb ) {
+    var files       = [],
+        iterated    = 0;
+
+    _getCourses( function () {
+        for ( var i = 0; i < total; i++ ) {
+            async.waterfall([
+                function ( callback ) {
+                    var skip        = chance.integer({
+                            min     : 0,
+                            max     : totalCourses - 1
+                        });
+
+                    Course.findOne().skip( skip ).exec( function ( err, course ) {
+                        callback( null, course );
+                    });
+                },
+                function ( course, callback ) {
+                    var date    = new Date( chance.hammertime() ),
+                        start   = new Date( course.start );
+
+                    date.setYear( start.getFullYear() );
+
+                    callback( null, course, date );
+                },
+                function ( course, date, callback ) {
+                    File.create({
+                        course  : course._id,
+                        date    : date.toISOString(),
+                        file    : chance.word({ syllables : 3 })
+                    }, function ( err, file ) {
+                        callback( null, file );
+                    });
+                }
+            ], function ( err, result ) {
+                if ( result ) {
+                    files.push( result );
+                }
+
+                if ( ++iterated == total ) {
+                    cb( files.length );
+                }
+            });
+        }
+    });
+};
