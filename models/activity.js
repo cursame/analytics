b78ec786363a9f90e9e72be64dea0a86cb4cbdc6
Carlos Cessa <carlos@pixative.com>
2016-04-05 15:58:43 -0500

    Activities Support
    - Created model to define an activity record in the database
    - Added `activities` router to handle the activities related requests

---
+++ models/activity.js
@@ -0,0 +1,29 @@
+var mongoose        = require( 'mongoose' ),
+    ActivitySchema  = new mongoose.Schema({
+        activity        : {
+            type        : String,
+            required    : true
+        },
+        course          : {
+            type        : mongoose.Schema.Types.ObjectId,
+            ref         : 'Course',
+            required    : true
+        },
+        date            : {
+            default     : Date.now,
+            type        : Date,
+            required    : true
+        },
+        teacher         : {
+            type        : mongoose.Schema.Types.ObjectId,
+            ref         : 'User',
+            required    : true
+        },
+        user            : {
+            type        : mongoose.Schema.Types.ObjectId,
+            ref         : 'User',
+            required    : true
+        }
+    });
+
+module.exports      = mongoose.model( 'Activity', ActivitySchema );
---
+++ routers/activities.js
@@ -0,0 +1,61 @@
+var express     = require( 'express' ),
+    router      = express.Router(),
+    Activity    = require( '../models/activity' ),
+    Session     = require( '../lib/session' ),
+    Utils       = require( '../lib/utils' );
+
+router.get( '/', Session.validate, function ( req, res, next ) {
+    var filters     = [ 'activity', 'course', 'date', 'teacher', 'user' ],
+        refs        = [
+            {
+                field   : 'course',
+                select  : 'description end name start'
+            },
+            {
+                field   : 'teacher',
+                select  : 'avatar email external_id name'
+            },
+            {
+                field   : 'user',
+                select  : 'avatar email external_id name'
+            }
+        ];
+
+    Utils.paginate( Activity, filters, refs, req, res, next );
+});
+
+router.post( '/', function ( req, res, next ) {
+    Activity.create({
+        activity    : req.body.activity,
+        course      : req.body.course,
+        date        : req.body.date,
+        teacher     : req.body.teacher,
+        user        : req.body.user
+    }, function ( err, file ) {
+        if ( err || !file ) {
+            err         = new Error( 'Invalid activity data' );
+            err.status  = 403;
+
+            next( err );
+        } else {
+            res.json( file );
+        }
+    });
+});
+
+router.delete( '/:id', Session.validate, function ( req, res, next ) {
+    Activity.findById( req.params.id, function ( err, activity ) {
+        if ( err || !activity ) {
+            err         = new Error( 'Invalid activity id' );
+            err.status  = 404;
+
+            next( err );
+        } else {
+            activity.remove( function () {
+                res.json( activity );
+            });
+        }
+    });
+});
+
+module.exports  = router;
